2de5118db71214736ea9c39b460e2b073da034e1
Breno Alves <brenoalv3ss@gmail.com>
2021-02-09 10:06:29 -0300

    Intentação

--- src/component/header.js
+++ src/component/header.js
@@ -11,43 +11,43 @@ class Header extends Component {
 		this.handleClick = this.handleClick.bind(this);
 	}
 
-	handleClick() {
-		const linkResume = document.createElement("a");
-		linkResume.download = `Breno's Resume`
-		linkResume.href =`${Resume}`;
-		linkResume.click()
-	}
+handleClick() {
+	const linkResume = document.createElement("a");
+	linkResume.download = `Breno's Resume`
+	linkResume.href =`${Resume}`;
+	linkResume.click()
+}
 
-	render() {
-		return (
-			<div className="header">
-				<div className="nav">
-					<button
-						type="button"
-						className="btn"
+render() {
+	return (
+		<div className="header">
+			<div className="nav">
+				<button
+					type="button"
+					className="btn"
+				>
+					<AnchorLink
+						className="link-home"
+						href="#home"
 					>
-						<AnchorLink
-							className="link-home"
-							href="#home"
-							>
-								HOME
-						</AnchorLink>
-					</button>
-					<div className="btn resume">
-						<input
-							className="btn btn-outline-danger btn-sm"
-							type="button"
-							value="FAÇA DOWNLOAD DO MEU CURRÍCULO"
-							onClick={ () => this.handleClick() }
-							/>
-					</div>
-					</div>
-					<div className="img">
-						<img className="img-header" src={ image } alt="Background Tecnologia"/>
-					</div>
-			</div>
-		)
-	}
+						HOME
+					</AnchorLink>
+				</button>
+				<div className="btn resume">
+					<input
+						className="btn btn-outline-danger btn-sm"
+						type="button"
+						value="FAÇA DOWNLOAD DO MEU CURRÍCULO"
+						onClick={ () => this.handleClick() }
+					/>
+				</div>
+				</div>
+				<div className="img">
+					<img className="img-header" src={ image } alt="Background Tecnologia"/>
+				</div>
+		</div>
+	)
+}
 }
 
 export default Header;
